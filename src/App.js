4ea06aaf023a6f1092b70404bc5f549dbc9548e8
Benjamin Dally <benjamin.dally@icloud.com>
2018-06-20 15:13:36 -0700

    added search form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/nav/Nav";
 import Layout from "./components/home/Layout";
+import Search from "./components/search/Search";
 import Results from "./components/results/Results";
 import Saved from "./components/saved-articles/Saved";
 
@@ -9,11 +10,12 @@ const App = () => (
   <Router>
     <div>
       <Nav />
-      {/* <Switch>
+      <Switch>
         <Route exact path="/" component={Layout} />
+        <Route exact path="/search" component={Search} />
         <Route exact path="/results" component={Results} />
         <Route exact path="/saved" component={Saved} />
-      </Switch> */}
+      </Switch>
     </div>
   </Router>
 );
---
+++ src/components/home/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Layout = () => (
+  <div className="text-center">
+    <h1 className="jumbotron">Welcome to a NYT article searcher and saver</h1>
+    <a className="badge badge-primary" href="/search">
+      Search
+    </a>
+  </div>
+);
+
+export default Layout;
